d08e95d5de56544243382bbacbfe9e4e892d434d
Arun Vishwakarma <45504815+ArunVishw@users.noreply.github.com>
2020-06-20 17:44:22 +0530

    Added Libraries, Configured Mongoose, and Schema
    Schema is added with password encryption function to encrypt password using bcrypt before data is pushed to database.

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express() ;
 const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const Admin = require('./models/adminSchema');
 app.use(cookieParser());
 app.use(express.json());
 
+
+mongoose.connect('mongodb://localhost:27017/interview_preparation', { useNewUrlParser: true, useUnifiedTopology: true},()=>{
+    console.log('Connected successfully to DATABASE')
+});
+
+const adminRouter = require('./routes/admin');
+app.use('/admin',adminRouter);
 
  app.listen(5000,()=>{
      console.log('Express server started')
---
+++ models/adminSchema.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const AdminSchema = new mongoose.Schema({
+    name :{
+        type : String,
+        required : true,
+        max : 20 
+    },
+    organization :{
+        type: String,
+        required: true,
+        max: 30 
+    },
+    email :{
+        type: String,
+        required: true,
+        max: 30 
+    },
+    password :{
+        type: String,
+        required: true,
+    }
+});
+
+AdminSchema.pre('save',function(next){
+    if(!this.isModified('password'))
+        return next();
+    bcrypt.hash(this.password,10,(err,passwordHash)=>{
+        if(err) return next(err);
+        this.password = passwordHash;
+        next();
+    });
+});
+
+AdminSchema.methods.comparePassword = function(pass,done){
+    bcrypt.compare(pass,this.password,(err,isMatch)=>{
+        if(err) return done(err);
+        else if(!isMatch)    return done(null,isMatch);
+        else return done(null,this);
+    });
+}
+
+module.exports = mongoose.model('Admin',AdminSchema);
